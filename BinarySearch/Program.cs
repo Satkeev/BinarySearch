be3748ce0a6ebdcf3cd0aafa8cecee9ed176048a
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-07-08 16:38:34 -0700

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BinarySearch
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] input1 = { 4, 8, 15, 16, 23, 42 };
+                int input2 = 15;
+                Console.WriteLine(BinarySearch(input1, input2));
+            }
+
+            static int BinarySearch(int[] arr, int searchVal)
+            {
+                int start = 0;
+                int end = arr.Length - 1;
+                do
+                {
+                    int midPoint = (start + end) / 2;
+                    Console.WriteLine($"start: {start}, end: {end}, midpoint: {midPoint}, arr[midpoint]: {arr[midPoint]}");
+                    if (arr[midPoint] == searchVal)
+                    {
+                        return midPoint;
+                    }
+                    else if (arr[midPoint] < searchVal)
+                    {
+                        start = midPoint + 1;
+                    }
+                    else
+                    {
+                        end = midPoint - 1;
+                    }
+                }
+                while (start <= end);
+                return -1;
+            }
+        }
+}
+
+    
+
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,48 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            int[] input1 = { 4, 8, 15, 16, 23, 42 };
+            int input2 = 15;
+            Console.WriteLine(BinarySearch(input1, input2));
+
+
+            static int BinarySearch(int[] arr, int searchVal)
+            {
+                int start = 0;
+                int end = arr.Length - 1;
+                do
+                {
+                    int midPoint = (start + end) / 2;
+                    Console.WriteLine($"start: {start}, end: {end}, midpoint: {midPoint}, arr[midpoint]: {arr[midPoint]}");
+                    if (arr[midPoint] == searchVal)
+                    {
+                        return midPoint;
+                    }
+                    else if (arr[midPoint] < searchVal)
+                    {
+                        start = midPoint + 1;
+                    }
+                    else
+                    {
+                        end = midPoint - 1;
+                    }
+                }
+                while (start <= end);
+                return -1;
+            }
+        }
+    }
+}
+
+
+
+
+ 
